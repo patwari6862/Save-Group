9c2a82edbfb9026874318bc184dbade4d1d2e250
Jagdeep gill [jatt life thuglife] <64634071+patwari6862@users.noreply.github.com>
2021-08-17 08:41:22 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,6 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH", None)
-    STRING_SESSION = os.environ.get("STRING_SESSION", ""
     FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", None)
     MONGODB_URI = os.environ.get("MONGODB_URI", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
